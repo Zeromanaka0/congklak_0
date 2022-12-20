6e03f0cc60d9e24893628e6742b2773237357dac
Zeromanaka0 <zeromanaka00@gmail.com>
2022-12-20 21:38:50 +0700

    first commit

---
+++ lib/biji.dart
@@ -0,0 +1,287 @@
+import 'package:flutter/material.dart';
+
+class Biji extends StatefulWidget {
+  const Biji({Key? key}) : super(key: key);
+
+  @override
+  State<Biji> createState() => _BijiState();
+}
+
+dynamic r1 = Colors.red;
+dynamic r2 = Colors.red;
+dynamic r3 = Colors.red;
+dynamic r4 = Colors.red;
+dynamic r5 = Colors.red;
+dynamic r6 = Colors.red;
+dynamic r7 = Colors.red;
+dynamic r8 = Colors.red;
+dynamic r9 = Colors.red;
+dynamic r10 = Colors.red;
+dynamic r11 = Colors.red;
+dynamic r12 = Colors.red;
+dynamic r13 = Colors.red;
+dynamic r14 = Colors.red;
+
+dynamic g1 = Colors.green;
+dynamic g2 = Colors.green;
+dynamic g3 = Colors.green;
+dynamic g4 = Colors.green;
+dynamic g5 = Colors.green;
+dynamic g6 = Colors.green;
+dynamic g7 = Colors.green;
+dynamic g8 = Colors.green;
+dynamic g9 = Colors.green;
+dynamic g10 = Colors.green;
+dynamic g11 = Colors.green;
+dynamic g12 = Colors.green;
+dynamic g13 = Colors.green;
+dynamic g14 = Colors.green;
+
+class _BijiState extends State<Biji> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: r1,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r2,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r3,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r4,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r5,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r6,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r7,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: r8,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r9,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r10,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r11,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r12,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r13,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: r14,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Biji2 extends StatefulWidget {
+  const Biji2({Key? key}) : super(key: key);
+
+  @override
+  State<Biji2> createState() => _Biji2State();
+}
+
+class _Biji2State extends State<Biji2> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: g1,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g2,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g3,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g4,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g5,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g6,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g7,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: g8,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g9,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g10,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g11,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g12,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g13,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: g14,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                width: 10,
+                height: 10,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
